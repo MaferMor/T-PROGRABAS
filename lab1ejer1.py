dcdf8ad196c7846ebc2880b617fbc18396d5768a
MaferMor <fernanda.moralesg@uanl.edu.mx>
2024-08-14 20:19:49 -0600

    tarea14Agosto

---
+++ lab1ejer1.py
@@ -0,0 +1,19 @@
+def max(a, b):
+    """
+    Devuelve el mayor de dos números.
+
+    Args:
+        a (int o float): Primer número.
+        b (int o float):Segundo número.
+    
+    Returns:
+        int o float: El mayor de los números.
+    """
+    if a > b:
+        return a 
+    else:
+        return b
+    
+valor = max(2,10)
+
+print('El numero mas grande es ',valor)
---
+++ lab1ejer2.py
@@ -0,0 +1,22 @@
+def max_de_tres(a, b, c):
+    """
+    Devuelve el mayor de tres números.
+
+    Args:
+        a (int o float): Primer número.
+        b (int o float): Segundo número.
+        c (int o float): Tercer número.
+
+    Returns:
+        int o float: El mayor de los tres números.
+    """
+    if a >= b and a >= c:
+        return a 
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c 
+
+valor = max_de_tres(10, 2, 3)
+
+print('El número mayor es: ', valor)
