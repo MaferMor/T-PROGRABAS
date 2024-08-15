007281c42486cf5a42a7673e87b85e5ffca4fad5
MaferMor <fernanda.moralesg@uanl.edu.mx>
2024-08-15 17:37:07 -0600

    terminado ejercicio 10

---
+++ lab1ejer10.py
@@ -0,0 +1,16 @@
+def histograma(lista):
+    """
+    Imprime un histograma en la pantalla utilizando asteriscos (*).
+
+    Args:
+        lista (list): Lista de números enteros.
+    """
+    for valor in lista:
+        print("*" * valor)
+
+
+
+def histograma(lista):
+     for valor in lista:
+        print("*" * valor)
+histograma([4, 9, 7])
