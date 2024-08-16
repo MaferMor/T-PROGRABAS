1582773e31b8b207a56567bcdeafb03ca3b8745e
MaferMor <fernanda.moralesg@uanl.edu.mx>
2024-08-16 17:25:09 -0600

    finish ejer 6

---
+++ lab2ajer6.py
@@ -0,0 +1,14 @@
+def calculate_ages():
+    current_year = int(input("Introduzca el año actual: "))
+
+    people = []
+    for i in range(3):
+        name = input(f"introduzaca el nombre de la persona {i+1}: ")
+        birth_year = int(input(f"Introduzca el año de nacimiento {name}: "))
+        people.append({"name": name, "birth_year": birth_year})  
+
+    for person in people:
+        age = current_year - person["birth_year"]
+        print(f"{person['name']} will turn {age} years old this year.")
+
+calculate_ages()
