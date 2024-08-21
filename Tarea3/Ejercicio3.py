1183feb20d0f6a1aa1e339e44f5038bd8b312ed2
MaferMor <fernanda.moralesg@uanl.edu.mx>
2024-08-21 12:35:47 -0600

    Finish ejer 3

---
+++ Tarea3/Ejercicio3.py
@@ -0,0 +1,14 @@
+frase = input("Ingrese una frase: ")
+
+mayusculas = frase.upper()
+
+minusculas = frase.lower()
+
+EraMayuscula = frase.title()
+
+print("Frase en mayúsculas:")
+print(mayusculas)
+print("\nFrase en minúsculas:")
+print(minusculas)
+print("\nFrase con la primera letra de cada palabra en mayúscula:")
+print(EraMayuscula)
