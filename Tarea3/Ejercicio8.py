0d3dc34caae6b8f4995cfa93f7be822593869e95
MaferMor <fernanda.moralesg@uanl.edu.mx>
2024-08-22 00:35:54 -0600

    Finish ejer 8

---
+++ Tarea3/Ejercicio8.py
@@ -0,0 +1,6 @@
+cadena1 = input("Ingrese la primera cadena de texto: ")
+cadena2 = input("Ingrese la segunda cadena de texto: ")
+
+cadena_concatenada = cadena1 + " " + cadena2
+
+print("La cadena concatenada es:", cadena_concatenada)
